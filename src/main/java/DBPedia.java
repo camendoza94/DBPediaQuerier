843a263fcda43221e5c0cd3f6187f15de94aa0a8
camendoza94 <camendoza94@gmail.com>
2017-04-26 22:55:48 -0500

    Avances

--- src/main/java/DBPedia.java
+++ src/main/java/DBPedia.java
@@ -1,32 +1,40 @@
 import org.apache.jena.query.*;
 
+import java.util.ArrayList;
+
 public class DBPedia {
-    DBPedia() {
-        String qs = "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"
-                + "PREFIX dbo:     <http://dbpedia.org/ontology/> \n"
-                + "prefix xsd:   <http://www.w3.org/2001/XMLSchema#> \n"
-                + "select distinct ?resource ?birth ?death ?nacionality where {\n"
-                + "  ?resource rdfs:label 'Fernando Botero'@en.\n"
-                + "  ?resource dbo:nationality ?nacionality.\n"
-                + "  ?resource dbo:birthDate ?b.\n"
-                + "  OPTIONAL {\n"
-                + "  ?resource dbo:deathDate ?d. \n"
-                + "}\n"
-                + "  BIND(year(xsd:date(?b)) as ?birth) .\n"
-                + "}";
+    public static void queryArtists(ArrayList<Artist> artistsVirtuoso) {
+        for (Artist artist: artistsVirtuoso) {
+            String qs = "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"
+                    + "PREFIX dbo:     <http://dbpedia.org/ontology/> \n"
+                    + "prefix xsd:   <http://www.w3.org/2001/XMLSchema#> \n"
+                    + "select distinct ?name ?birthYear ?nationality where {\n"
+                    + "  ?resource rdfs:label '" + artist.getName() + "' @en.\n"
+                    + "  ?resource dbo:nationality '" + artist.getNacionality() + "'.\n"
+                    + "  ?resource dbo:birthDate '" + artist.getBirthYear() + "'.\n"
+                    + "  OPTIONAL {\n"
+                    + "  ?resource dbo:deathDate ?d. \n"
+                    + "}\n"
+                    //+ "  BIND(year(xsd:date(?b)) as ?birth) .\n"
+                    + "}";
 
-        QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", QueryFactory.create(qs));
+            QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", QueryFactory.create(qs));
 
-        ResultSet results = exec.execSelect();
+            ResultSet results = exec.execSelect();
 
-//        while (results.hasNext()) {
-//
-//            System.out.println(results.next().get("").toString());
-//        }
+            while (results.hasNext()) {
+                QuerySolution next = results.next();
+                String name = next.get("name").toString().split("\\^")[0];
+                String nationality = next.get("nationality").toString().split("\\^")[0];
+                int birthYear = Integer.parseInt(next.get("birthyear").toString().split("\\^")[0]);
+                Artist dbpediaArtist = new Artist(name, nationality, birthYear);
+            }
 
-        ResultSetFormatter.out(results);
+            ResultSetFormatter.out(results);
+        }
     }
-    public static void main(String[] args) {
-       DBPedia main = new DBPedia();
-    }
+
+    /*public static void main(String[] args) {
+        DBPedia main = new DBPedia();
+    }*/
 }
--- src/main/java/Virtuoso.java
+++ src/main/java/Virtuoso.java
@@ -13,24 +13,24 @@ public class Virtuoso {
         String qs = "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"
                 + "prefix xsd:   <http://www.w3.org/2001/XMLSchema#> \n"
                 + "prefix : <http://www.grupo1.semanticweb.uniandes.edu.co/curso/arte#> \n"
-                + "select distinct ?name ?birthyear ?birthplace where {\n"
+                + "select ?name ?birthyear ?nationality where {\n"
                 + "?resource a :Artist.\n"
                 + "?resource :name ?name.\n"
                 + "?resource :birthYear ?birthyear.\n"
-                + "?resource :birthPlace ?birthplace.\n"
+                + "?resource :nationality ?nationality.\n"
                 + "}";
 
         QueryExecution exec = QueryExecutionFactory.sparqlService("http://172.24.42.79:8890/sparql", QueryFactory.create(qs));
 
         ResultSet results = exec.execSelect();
 
-        ResultSetFormatter.out(results);
+        //ResultSetFormatter.out(results);
         while(results.hasNext()) {
             QuerySolution next = results.next();
-            String name = next.get("name").toString().split("^^")[0];
-            String nacionality = next.get("birthplace").toString().split("^^")[0];
-            int birthYear = Integer.parseInt(next.get("birthyear").toString().split("^^")[0]);
-            artists.add(new Artist(name, nacionality, birthYear));
+            String name = next.get("name").toString().split("\\^")[0];
+            String nationality = next.get("nationality").toString().split("\\^")[0];
+            int birthYear = Integer.parseInt(next.get("birthyear").toString().split("\\^")[0]);
+            artists.add(new Artist(name, nationality, birthYear));
         }
 
         return artists;
@@ -38,5 +38,6 @@ public class Virtuoso {
     public static void main(String[] args) {
         ArrayList<Artist> artistas = Virtuoso.queryArtists();
         System.out.println(artistas.size());
+
     }
 }
